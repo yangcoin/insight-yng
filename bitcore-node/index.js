2f81479f830413e90d3e3678ea4ed2377ae64a89
user <>
2017-11-07 15:01:09 +0900

    change depends

--- bitcore-node/index.js
+++ bitcore-node/index.js
@@ -7,53 +7,54 @@ var exec = require('child_process').exec;
 var pkg = require('../package.json');
 
 var InsightUI = function(options) {
-  BaseService.call(this, options);
-  this.apiPrefix = options.apiPrefix || 'api';
-  this.routePrefix = options.routePrefix || '';
+    BaseService.call(this, options);
+    this.apiPrefix = options.apiPrefix || 'api';
+    this.routePrefix = options.routePrefix || '';
 };
 
-InsightUI.dependencies = ['insight-api'];
+InsightUI.dependencies = ['insight-yng-api'];
 
 inherits(InsightUI, BaseService);
 
 InsightUI.prototype.start = function(callback) {
 
-  var self = this;
-  pkg.insightConfig.apiPrefix = self.apiPrefix;
-  pkg.insightConfig.routePrefix = self.routePrefix;
+    var self = this;
+    pkg.insightConfig.apiPrefix = self.apiPrefix;
+    pkg.insightConfig.routePrefix = self.routePrefix;
 
-  fs.writeFileSync(__dirname + '/../package.json', JSON.stringify(pkg, null, 2));
-  exec('cd ' + __dirname + '/../;' +
-    ' npm run install-and-build', function(err) {
-    if (err) {
-      return callback(err);
-    }
-    self.indexFile = self.filterIndexHTML(fs.readFileSync(__dirname + '/../public/index-template.html', {encoding: 'utf8'}));
-    callback();
-  });
+    fs.writeFileSync(__dirname + '/../package.json', JSON.stringify(pkg, null, 2));
+    exec('cd ' + __dirname + '/../;' +
+        ' npm run install-and-build',
+        function(err) {
+            if (err) {
+                return callback(err);
+            }
+            self.indexFile = self.filterIndexHTML(fs.readFileSync(__dirname + '/../public/index-template.html', { encoding: 'utf8' }));
+            callback();
+        });
 
 };
 
 InsightUI.prototype.getRoutePrefix = function() {
-  return this.routePrefix;
+    return this.routePrefix;
 };
 
 InsightUI.prototype.setupRoutes = function(app, express) {
-  var self = this;
-  app.use(express.static(__dirname + '/../public'));
-  // if not in found, fall back to indexFile (404 is handled client-side)
-  app.use(function(req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(self.indexFile);
-  });
+    var self = this;
+    app.use(express.static(__dirname + '/../public'));
+    // if not in found, fall back to indexFile (404 is handled client-side)
+    app.use(function(req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(self.indexFile);
+    });
 };
 
 InsightUI.prototype.filterIndexHTML = function(data) {
-  var transformed = data;
-  if (this.routePrefix !== '') {
-    transformed = transformed.replace('<base href="/"', '<base href="/' + this.routePrefix + '/"');
-  }
-  return transformed;
+    var transformed = data;
+    if (this.routePrefix !== '') {
+        transformed = transformed.replace('<base href="/"', '<base href="/' + this.routePrefix + '/"');
+    }
+    return transformed;
 };
 
 module.exports = InsightUI;
