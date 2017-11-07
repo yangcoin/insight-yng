7fe6e072993fe360712431af4436afe81fe844a5
user <user@users-iMac.local>
2017-11-07 14:53:54 +0900

    change prefix

--- public/src/js/services/api.js
+++ public/src/js/services/api.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('insight.api')
-    .factory('Api',
-        function() {
-            return {
-                apiPrefix: '/insight-yng-api'
-            }
-        });
+  .factory('Api',
+    function() {
+      return {
+        apiPrefix: '/insight-yng-api'
+      }
+    });
